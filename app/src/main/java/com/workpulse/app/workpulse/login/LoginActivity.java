05eae9f467f558c699ede79a2e7095edcd3770c0
Ambar <ambar.jain@workpulse.com>
2020-11-20 18:04:22 +0530

    Initial Commit

--- app/src/main/java/com/workpulse/app/workpulse/login/LoginActivity.java
+++ app/src/main/java/com/workpulse/app/workpulse/login/LoginActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.workpulse.app.workpulse.loginlibrary.Toaster;
+
 public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        new Toaster(this);
     }
 }
